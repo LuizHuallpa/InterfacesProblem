139f1966ef1ae56e70c46e3c346e0fc3126bc1b8
018421631 <018421631@IBM-PF32FCC2>
2022-09-19 17:13:54 -0300

    Add project files.

---
+++ InterfacesProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using InterfacesProblem.Entities;
+using InterfacesProblem.Services;
+using System.Globalization;
+
+namespace InterfacesProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractServices contractService = new ContractServices(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
